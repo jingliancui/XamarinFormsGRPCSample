f997bf3a62e3b7e0b18a99950ce9905632916ac0
jingliancui <ericcui@qq.com>
2020-03-11 14:31:54 +0800

    Add sample code to github

---
+++ GrpcSample/SampleApp/SampleApp/SampleApp/App.xaml.cs
@@ -0,0 +1,66 @@
+﻿using Grpc.Core;
+using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamarinGrpcSampleServer;
+
+namespace SampleApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            try
+            {
+                var server = new Server
+                {
+                    Services = { XamarinGrpcSampleServer.Greeter.BindService(new GreeterImpl()) },
+
+                    Ports = { new ServerPort("0.0.0.0", 54321, ServerCredentials.Insecure) }
+                };
+
+                server.Start();
+            }
+            catch (Exception ex)
+            {
+                var b = ex;
+            }
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+
+    public class GreeterImpl : XamarinGrpcSampleServer.Greeter.GreeterBase
+    {
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            var currentPlatform = Xamarin.Essentials.DeviceInfo.Platform;
+            var platform = "";
+            if (currentPlatform == Xamarin.Essentials.DevicePlatform.Android)
+            {
+                platform = "android";
+            }
+            if (currentPlatform == Xamarin.Essentials.DevicePlatform.iOS)
+            {
+                platform = "ios";
+            }
+            MessagingCenter.Send(new object(), "receivedmsg", request);
+            return Task.FromResult(new HelloReply { Message = platform + ": Hello " + request.Name });
+        }
+    }
+}
---
+++ GrpcSample/SampleApp/SampleApp/SampleApp/MainPage.xaml.cs
@@ -0,0 +1,71 @@
+﻿using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SampleApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainViewModel();
+            MessagingCenter.Subscribe<object, XamarinGrpcSampleServer.HelloRequest>(this, "receivedmsg", (o, h) =>
+            {
+                Xamarin.Essentials.MainThread.BeginInvokeOnMainThread(() => 
+                {
+                    var viewModel = BindingContext as MainViewModel;
+                    viewModel.MsgList.Add(new MsgModel { Msg = h.Name });
+                });
+            });
+        }
+
+        Channel channel;
+
+        private void SendBtn_Clicked(object sender, EventArgs e)
+        {
+            var viewModel = BindingContext as MainViewModel;
+
+            string user = MsgInput.Text;
+
+            var client = new XamarinGrpcSampleClient.Greeter.GreeterClient(channel);
+
+            var reply = client.SayHello(new XamarinGrpcSampleClient.HelloRequest { Name = user });
+
+            if (!string.IsNullOrEmpty(reply.Message))
+            {
+                viewModel.MsgList.Add(new MsgModel { Msg = $"target had read." });
+            }
+            
+        }
+
+        private void ConnectBtn_Clicked(object sender, EventArgs e)
+        {
+            channel = new Channel(IpInput.Text, ChannelCredentials.Insecure);            
+        }
+    }
+    public class MainViewModel
+    {
+        public MainViewModel()
+        {
+            MsgList = new ObservableCollection<MsgModel>();
+        }
+
+        public ObservableCollection<MsgModel> MsgList { get; set; }
+    }
+
+    public class MsgModel
+    {
+        public string Msg { get; set; }
+    }
+
+}
